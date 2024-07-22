ebe4060c0062f3f2de997aa2cdc84de7038d9d96
kazurayam <kazuaki.urayama@gmail.com>
2024-07-22 16:33:53 +0900

    #21 coined v0.4.2

---
+++ docs/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.kazurayam.timekeeper"},{"l":"com.kazurayam.timekeeper.recordcomparator"},{"l":"com.kazurayam.timekeeper.reporter"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
